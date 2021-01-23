c61f57aa3a41b6f01884a1fe6a0eef5609349287
iqfareez <iqmal3@outlook.com>
2021-01-23 09:30:33 +0800

    Some changes to 1.1.1+2

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:is_2020_end/animated_flip_counter.dart';
 
 class MyHomePage extends StatefulWidget {
   final now = DateTime.now();
@@ -8,25 +9,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int phoneDate;
-  int targetDate = 2021;
+  int deviceYear;
+  int targetYear = 2021;
   String answer;
+  int countdown = 0;
 
   @override
   void initState() {
     super.initState();
-    var formatter = DateFormat('y').format(widget.now);
-    phoneDate = int.parse(formatter);
+    var yearFormatter = DateFormat('y').format(widget.now);
+    deviceYear = int.parse(yearFormatter);
   }
 
   bool checkYear() {
-    return phoneDate > targetDate ? true : false;
+    return deviceYear > targetYear ? true : false;
+  }
+
+  int findDifference() =>
+      widget.now.difference(DateTime(targetYear + 1)).inDays;
+
+  void incrementNumber() {
+    for (var i = 1; i < findDifference(); i++) {
+      setState(() {
+        countdown = i;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    answer = checkYear() ? 'Yes.' : 'Not yet.';
-
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      setState(() {
+        countdown = findDifference();
+      });
+    });
+    answer = checkYear() ? 'Yes.' : 'Not yet :")';
+    incrementNumber();
     return Scaffold(
       body: Column(
         children: [
@@ -35,13 +53,25 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Container(
               color: Theme.of(context).primaryColor,
               child: Center(
-                child: Text(
-                  'Is $targetDate end?',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 35,
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Is ',
+                    style: TextStyle(fontSize: 35),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: targetYear.toString(),
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(text: ' end?'),
+                    ],
                   ),
                 ),
+                // child: Text(
+                //   'Is $targetYear end?',
+                //   style: TextStyle(
+                //     color: Colors.white,
+                //     fontSize: 35,
+                //   ),
+                // ),
               ),
             ),
           ),
@@ -55,12 +85,30 @@ class _MyHomePageState extends State<MyHomePage> {
                     answer,
                     style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
                   ),
-                  checkYear()
+                  SizedBox(
+                    height: 10,
+                  ),
+                  !checkYear()
+                      ? Container(
+                          padding: const EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                              color: Colors.black87,
+                              border: Border.all(),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20))),
+                          child: AnimatedFlipCounter(
+                            value: countdown,
+                            color: Colors.white,
+                            duration: Duration(
+                                seconds: findDifference() < 100 ? 4 : 7),
+                            size: 85,
+                          ),
+                        )
+                      : Container(),
+                  !checkYear()
                       ? Text(
-                          answer,
-                          //TODO: Do rolling text here
-                          style: TextStyle(
-                              fontSize: 45, fontWeight: FontWeight.bold),
+                          'days left',
+                          style: TextStyle(fontWeight: FontWeight.w300),
                         )
                       : Container(),
                 ],
